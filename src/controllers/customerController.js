148b2afb32a7377ac96c647f261c460f9dbba319
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2022-05-07 00:29:48 -0500

    list and delete info db

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -18,8 +18,16 @@ controller.save = (req,res)=>{
     req.getConnection((err,conn)=>{
         conn.query('INSERT INTO customer set ?',[data],(err,customer)=>{
             console.log(customer);
-            res.send('INSERTED');
+            res.redirect('/');
         });
     })
 };
+controller.delete = (req,res)=>{
+    const {id} = req.params;
+    req.getConnection((err,conn)=>{
+        conn.query('DELETE FROM customer WHERE id = ?',[id],(err,rows)=>{
+            res.redirect('/');
+        });
+    });
+}
 module.exports = controller;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 router.get('/',customerController.list);
 router.post('/add',customerController.save);
+router.get('/delete/:id',customerController.delete)
 module.exports= router;
